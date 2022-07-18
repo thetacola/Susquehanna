a83e679371b15c9bff81868539ad1f29c1be524c
N3ther <thenetherreactor@gmail.com>
2022-07-18 05:14:50 -0400

    changed logo and added more version info

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -2,15 +2,19 @@ package gui;
 
 import java.net.URISyntaxException;
 
+import com.pixelduke.control.Ribbon;
+import com.pixelduke.control.ribbon.RibbonGroup;
 import com.pixelduke.control.ribbon.RibbonTab;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -27,14 +31,40 @@ public class App extends Application {
     public void start(Stage stage) {
         var javaVersion = System.getProperty("java.version");
 
-        Label versionLabel = new Label("Running on Java " + javaVersion + ".");
+        Label javaVersionLabel = new Label("Running on Java " + javaVersion + ".");
+        Label javaFXVersionLabel = new Label("Bundled with JavaFX SDK 18.0.1.");
+        Label algonquinVersionLabel = new Label("Bundled with AlgonquinTTS 0.2.1.");
+        Label versionLabel = new Label("Version 0.0.1 \"Otsego\", build 22w29a ***SNAPSHOT VERSION***");
+        javaVersionLabel.setTextFill(Color.WHITE);
+        javaFXVersionLabel.setTextFill(Color.WHITE);
+        algonquinVersionLabel.setTextFill(Color.WHITE);
         versionLabel.setTextFill(Color.WHITE);
         Image bannerLogo = new Image(App.class.getResourceAsStream("img/bannerlogo.png"));
+        Image hoveredBannerLogo = new Image(App.class.getResourceAsStream("img/bannerlogo-hover.png"));
         ImageView bannerLogoView = new ImageView(bannerLogo);
+        bannerLogoView.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent mouse) {
+				bannerLogoView.setImage(hoveredBannerLogo);
+			}
+        	
+        });
+        bannerLogoView.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent mouse) {
+				bannerLogoView.setImage(bannerLogo);
+			}
+        	
+        });
         
-        RibbonTab ribbon = new RibbonTab();
         
-        VBox mainVBox = new VBox(bannerLogoView, versionLabel);
+        
+        RibbonTab home = new RibbonTab();
+        Ribbon ribbon = new Ribbon();
+        
+        VBox mainVBox = new VBox(bannerLogoView, javaVersionLabel, javaFXVersionLabel, algonquinVersionLabel, versionLabel);
         mainVBox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(mainVBox);
         stage.setScene(scene);
