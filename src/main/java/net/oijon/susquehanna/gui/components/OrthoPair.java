2a9f44fe4e19a83ed1784e7d7046219c859fdd65
thetacola <alex@consus.fun>
2024-12-13 04:35:17 -0500

    added comments to gui.components.OrthoPair

--- src/main/java/net/oijon/susquehanna/gui/components/OrthoPair.java
+++ src/main/java/net/oijon/susquehanna/gui/components/OrthoPair.java
@@ -6,6 +6,11 @@ import javafx.scene.layout.HBox;
 
 public class OrthoPair extends HBox {
 
+	/**
+	 * Creates an OrthoPair, a component for displaying pairs of orthographic information.
+	 * @param left The text that should go on the left
+	 * @param right The text that should go on the right
+	 */
 	public OrthoPair(String left, String right) {
 		Label leftLabel = new Label(left);
 		Label colonLabel = new Label(":");
