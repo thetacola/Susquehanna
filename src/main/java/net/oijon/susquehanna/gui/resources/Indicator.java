dc27ee799fe6cc591dd05f2e44096ffc7e0ba996
thetacola <alex@consus.fun>
2024-12-14 00:56:40 -0500

    added loaders to gui.resources.* to cut down on unneeded vars

--- src/main/java/net/oijon/susquehanna/gui/resources/Indicator.java
+++ src/main/java/net/oijon/susquehanna/gui/resources/Indicator.java
@@ -6,18 +6,16 @@ import javafx.scene.image.ImageView;
 public class Indicator {
 
 	// images
-	private static Image fileIndicator = new Image(Indicator.class.getResourceAsStream("/img/file-bar.png"));
-	private static Image phonologyIndicator = new Image(Indicator.class.getResourceAsStream("/img/phonology-bar.png"));
-	private static Image orthographyIndicator = new Image(Indicator.class.getResourceAsStream("/img/orthography-bar.png"));
-	private static Image grammarIndicator = new Image(Indicator.class.getResourceAsStream("/img/grammar-bar.png"));
-	private static Image lexiconIndicator = new Image(Indicator.class.getResourceAsStream("/img/lexicon-bar.png"));
-	private static Image settingsIndicator = new Image(Indicator.class.getResourceAsStream("/img/settings-bar.png"));
+	
+	private static Image loadImage(String name) {
+		return new Image(Indicator.class.getResourceAsStream("/img/" + name));
+	}
 	
 	// imageviews
-	public static ImageView FILE = new ImageView(fileIndicator);
-	public static ImageView PHONOLOGY = new ImageView(phonologyIndicator);
-	public static ImageView ORTHOGRAPHY = new ImageView(orthographyIndicator);
-	public static ImageView GRAMMAR = new ImageView(grammarIndicator);
-	public static ImageView LEXICON = new ImageView(lexiconIndicator);
-	public static ImageView SETTINGS = new ImageView(settingsIndicator);
+	public static ImageView FILE = new ImageView(loadImage("file-bar.png"));
+	public static ImageView PHONOLOGY = new ImageView(loadImage("phonology-bar.png"));
+	public static ImageView ORTHOGRAPHY = new ImageView(loadImage("orthography-bar.png"));
+	public static ImageView GRAMMAR = new ImageView(loadImage("grammar-bar.png"));
+	public static ImageView LEXICON = new ImageView(loadImage("lexicon-bar.png"));
+	public static ImageView SETTINGS = new ImageView(loadImage("settings-bar.png"));
 }
