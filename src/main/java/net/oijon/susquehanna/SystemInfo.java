ba7d9b72c0713b308fcd222c70de15a83fbedd5e
thetacola <alex@consus.fun>
2026-08-06 18:53:42 -0400

    version bump

--- src/main/java/net/oijon/susquehanna/SystemInfo.java
+++ src/main/java/net/oijon/susquehanna/SystemInfo.java
@@ -65,7 +65,7 @@ public final class SystemInfo {
      */
     public static String buildName() {
     	if (isSnapshot()) {
-    		return "26w32a";
+    		return "26w32b";
     	} else {
     		return susquehannaVerName() + ", " + susquehannaVerNum();
     	}
